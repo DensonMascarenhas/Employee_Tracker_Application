0bcd3db01d248831fa0a3db4a9d8238598e8d229
Mascarenhas <dmascare@Densons-MBP.paychex.com>
2023-01-06 12:01:28 +0530

    small change

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :employees, :controllers=>{:sessions=>'employees/sessions',:invitations=>'employees/invitations'}
+  devise_for :employees, :controllers=>{:sessions=>'employees/sessions',:invitations=>'employees/invitations',:registrations=>'employees/registrations'}
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
